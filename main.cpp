8b3a785326b73e9891fdc1002a7e4bd972044b37
naezith <tolgaay@live.com>
2016-06-06 01:47:14 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include "NetworkManager.h"
+
+using namespace std;
+
+int main(int argc, char * argv[]){
+    SetConsoleTitle("Remnants of Naezith - Patcher");
+    // Close if it's opened manually
+    if(argc < 4 || strcmp(argv[1], "update") != 0){
+        ShellExecute(GetDesktopWindow(), "open", "naezith.exe", "", NULL, SW_SHOWNORMAL);
+        return 0;
+    }
+    int version = atoi(argv[2]);
+    int versionInServer = atoi(argv[3]);
+    if(version >= versionInServer){
+        std::cout << "IMPORTANT: This is something not expected, contact the developer twitter.com/naezith about this. " <<
+        "There probably happened a big update which patcher can't update properly. " <<
+        "He will probably ask you to download the whole game again." << std::endl;
+        cin.get(); return -1;
+    }
+    cout << "\n Your client needs update. ARGS: "  << argv[0] << " " << argv[1] << endl;
+    cout << "\n\n Current Version : " << version << endl;
+    cout << "\n Target Version : " << versionInServer << endl;
+
+    cout << "\n\n Checking the difference between versions..." << endl;
+    // Get file paths and actions
+    if(NetworkManager::i().fetchChanges(version) != NetworkManager::SUCCEED){
+        cout << " FetchChanges failed!" << endl;
+        cin.get(); return -1;
+    }
+
+    // Do actions for paths
+    cout << " -> Executing Actions..." << endl;
+    for(int i = 0; i < NetworkManager::i().change_count; ++i){
+        ACTION& act = NetworkManager::i().files[i];
+        std::cout << "File : " << act.path << " , Action : " << act.action << std::endl;
+        if(act.path != "patcher.exe" && act.path.find("replays") == string::npos){
+            std::string pathToRemove = ".\\" + act.path;
+            ifstream fin(pathToRemove.c_str());
+            if (fin){
+                cout << "File found, deleting..." << endl;
+                fin.close();
+                if(remove(pathToRemove.c_str()) != 0){
+                    cout << " Could not remove " << pathToRemove << endl;
+                    cin.get(); exit(-1);
+                }
+            }
+            if(act.action == "add"){
+                if(!NetworkManager::i().downloadFile(NetworkManager::i().hostIP, act.path, "")){
+                    cin.get(); exit(-1);
+                }
+            }
+        }
+    }
+
+    // Run the game
+    ofstream out("data/version.rond", ios::out);
+    out << versionInServer;
+    out.close();
+    cout << " -> Update successful, Launching the game..." << endl;
+    ShellExecute(GetDesktopWindow(), "open", "naezith.exe", "", NULL, SW_SHOWNORMAL);
+    return 0;
+}
---
+++ src/NetworkManager.cpp
@@ -0,0 +1,82 @@
+#include <fstream>
+#include "NetworkManager.h"
+#include "depend/picojson.h"
+
+NetworkManager::NetworkManager():
+    http("http://ron.doomdns.com/", 3000) {
+}
+NetworkManager::NETWORKSTATUS NetworkManager::fetchChanges(const int version) {
+    std::ostringstream data;
+    data << "version=" << version;
+    std::cout << "Fetching changes..." << std::endl;
+    NetworkManager::NETWORKSTATUS status = postData("fetchChanges", data);
+    change_count = 0;
+    picojson::value respond;
+    std::string err = picojson::parse(respond, res.getBody());
+    if (!err.empty()) std::cerr << err << std::endl;
+    else {
+        const picojson::value::object& obj = respond.get<picojson::object>();
+        for (picojson::value::object::const_iterator i = obj.begin(); i != obj.end(); ++i) {
+            if(i->first == "status") status = (NetworkManager::NETWORKSTATUS)atoi(i->second.to_str().c_str());
+            else if(i->first == "changes") {
+                picojson::value::array changes = i->second.get<picojson::array>();
+                change_count = changes.size();
+                for(unsigned i = 0; i < changes.size(); ++i) {
+                    files[i].action = changes[i].get("action").to_str();
+                    files[i].path = changes[i].get("path").to_str();
+                }
+                std::cout << " There are " << change_count << " files have been changed." << std::endl;
+            }
+        }
+    }
+    return status;
+}
+
+bool NetworkManager::downloadFile(std::string server, std::string file, std::string dest){
+    std::cout << " Downloading : " << file;
+    // Download file
+    sf::Http http;
+	http.setHost(server);
+	sf::Http::Request request;
+	request.setMethod(sf::Http::Request::Get);
+	request.setUri(std::string("Release/" + file));
+	sf::Http::Response response = http.sendRequest(request);
+	if(response.getStatus() != sf::Http::Response::Ok){
+        std::cout << " -> Failed!" << std::endl;
+		return false;
+    }
+
+	std::string body = response.getBody();
+
+
+
+    dest.append(file);
+    std::cout << " -> Saved!" << std::endl;
+
+    std::ofstream out(dest.c_str(), std::ios::out | std::ios::binary);
+    if(!out.is_open()){
+        std::cout << " Could not save the file." << std::endl;
+        return false;
+    }
+    out.write(body.data(), sizeof(char)*body.size());
+    out.close();
+	return true;
+}
+
+
+NetworkManager::NETWORKSTATUS NetworkManager::postData(const std::string& request, const std::ostringstream& data) {
+    sf::Http::Request req("/" + request, sf::Http::Request::Post);
+    req.setBody(data.str());
+
+    res = http.sendRequest(req);
+
+    return res.getStatus() == sf::Http::Response::Ok ? (NetworkManager::NETWORKSTATUS)atoi(res.getBody().c_str()) : CONNECTION_ERROR;
+}
+
+NetworkManager::NETWORKSTATUS NetworkManager::getData(const std::string& request) {
+    sf::Http::Request req("/" + request, sf::Http::Request::Get);
+
+    res = http.sendRequest(req);
+
+    return res.getStatus() == sf::Http::Response::Ok ? (NetworkManager::NETWORKSTATUS)atoi(res.getBody().c_str()) : CONNECTION_ERROR;
+}
